07d45880d1b867ce17a283b2209190ede524e2f1
KingMilly15 <bcboy0015@gmail.com>
2025-01-14 16:18:48 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "Smartcash"
 
-global.banknumber = "8133729715"
+global.banknumber = "9025286907"
 
-global.bankowner = "FADARE"
+global.bankowner = "Peace Godfred"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
